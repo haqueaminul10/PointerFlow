9bb10522282cc882587eab85e121de02bc2fc139
haqueaminul10 <haqueaminul10100@gmail.com>
2025-01-16 03:53:00 +0600

    our story done

---
+++ index.js
@@ -0,0 +1,23 @@
+async function loadComponent(id, filePath) {
+  try {
+    const response = await fetch(filePath);
+
+    if (!response.ok) {
+      throw new Error(`Failed to load ${filePath}`);
+    }
+
+    const data = await response.text();
+    document.getElementById(id).innerHTML = data;
+  } catch (err) {
+    console.error('Error loading component:', err);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadComponent('hero', './Components/hero/hero.html');
+  loadComponent(
+    'product-category',
+    './Components/product category/productCategory.html'
+  );
+  loadComponent('ourStory', './Components/ourStory/ourStory.html');
+});
